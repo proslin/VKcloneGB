81916abb1e3e1afdbcdb7e944ab31bd8681cbb41
Vitaly Prosvetov <vp12@yandex.ru>
2022-02-14 10:56:42 +0300

    Initial Commit

---
+++ vkGeekBrains/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkGeekBrains
+//
+//  Created by Vitaly Prosvetov on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
